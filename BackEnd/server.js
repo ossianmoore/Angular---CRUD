b0edc07a2f6329175813a42f3868c9212b8486ab
Martin Kenirons <martin.kenirons@gmit.ie>
2019-10-29 16:10:03 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const mongoDB = 'mongodb://admin:admin123#@ds241408.mlab.com:41408/dm_lab7'
+mongoose.connect(mongoDB, {useNewUrlParser:true});
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title:String,
+  year:String,
+  poster:String
+});
+
+const MovieModel = mongoose.model('movie',movieSchema);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,26 +38,30 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/movies', (req,res,next) => {
-  const movies = [
-    {
-      "Title": "Avengers: Infinity War",
-      "Year": "2018",
-      "imdbID": "tt4154756",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Charlie Wilson's War",
-      "Year": "2007",
-      "imdbID": "tt0472062",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-    }];
+  // const movies = [
+  //   {
+  //     "Title": "Avengers: Infinity War",
+  //     "Year": "2018",
+  //     "imdbID": "tt4154756",
+  //     "Type": "movie",
+  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+  //   },
+  //   {
+  //     "Title": "Charlie Wilson's War",
+  //     "Year": "2007",
+  //     "imdbID": "tt0472062",
+  //     "Type": "movie",
+  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
+  //   }];
   console.log("get request")
-  res.json({
-    message: 'Posts fetched succesfully!',
-    movies: movies
-  });
+  MovieModel.find((err,data)=>{
+    res.json({movies:data});
+  })
+  
+  // res.json({
+  //   message: 'Posts fetched succesfully!',
+  //   movies: movies
+  // });
 })
 
 app.post('/api/movies', (req,res) =>{
@@ -52,9 +70,24 @@ console.log(req.body)
 console.log(req.body.title);
 console.log(req.body.year);
 console.log(req.body.poster);
+
+MovieModel.create({
+  title: req.body.title,
+  year: req.body.year,
+  poster: req.body.poster
+});
+res.json('data uploaded')
+
+
 })
 
+app.get('/api/movies/:id',(req,res)=>{
+  console.log(req.params.id);
 
+  MovieModel.findById(req.params.id, (err, data)=>{
+    res.json(data);
+  })
+})
 
 app.listen(PORT, function () {
   console.log('Server is running on Port: ', PORT);
